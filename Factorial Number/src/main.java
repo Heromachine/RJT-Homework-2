25383961fd5ac2e571212d6569c1afabb0aff427
HEROMACHINE <Jesse.reyna01@utrgv.edu>
2018-08-26 22:27:08 -0500

    Inital commit

---
+++ Factorial Number/src/main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class main {
+
+	public static void main(String[] args) 
+	{	
+		Scanner sc = new Scanner (System.in); 
+		
+		int n, i; 
+		int fact = 1; 
+		
+		System.out.println("Enter a Number");
+		n = sc.nextInt();
+		
+		for (i = 1; i <= n; i++ )
+		{
+			fact = fact * i; 
+		}
+			
+		System.out.print("Factorial of "+ n +" is " + fact);
+	}
+
+}
