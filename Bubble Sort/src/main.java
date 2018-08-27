a317eafffc965f5e8964f99fbb36bc960da92591
HEROMACHINE <Jesse.reyna01@utrgv.edu>
2018-08-26 23:00:49 -0500

    Init Commit

---
+++ Bubble Sort/src/main.java
@@ -0,0 +1,56 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int intarray[] = {9, 3, 7, 2, 1};
+		
+		System.out.println("\tBefore Sort: ");
+		for (int i: intarray)
+		{
+			System.out.print(i + ", ");
+		}
+		
+		System.out.println("\n--------------\n ");
+		bubblesort(intarray);
+		
+		System.out.println("\tAfter Sort: ");
+		for (int i: intarray)
+		{
+			System.out.println(i + ", ");
+		}
+	
+	}
+	
+	
+	
+	private static void bubblesort(int[] intarray)
+	{
+		int temp = 0;
+		
+		for (int i = 0; i < intarray.length; i++)
+		{
+			
+			for (int j =1; j < (intarray.length)-i; j++)
+			{
+				
+				if (intarray[j - 1] > intarray[j])
+				{
+					temp = intarray[j-1];
+					intarray[j-1]= intarray[j]; 
+					intarray[j] = temp;
+					
+					for (int x: intarray)
+					{
+						System.out.print(x + ", ");
+					}
+					
+					System.out.println("\n-------------- ");
+				}
+			}
+		}
+		
+	}
+}
