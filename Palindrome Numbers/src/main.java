c31507e5e92e96a360ee97ac1d8cb606ba2739ac
HEROMACHINE <Jesse.reyna01@utrgv.edu>
2018-08-26 22:47:10 -0500

    Init Commit

---
+++ Palindrome Numbers/src/main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner; 
+
+public class main {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Enter Range: ");
+		int r = sc.nextInt();
+		
+		for (int i = 0; i <= r; i++)
+		{	
+			int n = i; 			
+			int temp = n, reverse = 0;
+			
+			while (temp != 0)
+			{
+				reverse = reverse * 10; 
+				reverse = reverse + temp%10;
+				temp = temp/10; 
+			}
+			
+			if (n == reverse)
+				System.out.println(n + ", "); 
+		}
+	}
+
+}
