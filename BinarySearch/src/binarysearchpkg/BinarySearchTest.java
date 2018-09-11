e9e243d42a63e57779e8588795bd40e0800f6a2c
HEROMACHINE <Jesse.reyna01@utrgv.edu>
2018-09-10 21:07:24 -0500

    update

--- BinarySearch/src/binarysearchpkg/BinarySearchTest.java
+++ BinarySearch/src/binarysearchpkg/BinarySearchTest.java
@@ -1,5 +1,38 @@
 package binarysearchpkg;
 
-public class BinarySearchTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTest {
+	
+	
+	@Test 
+	public void binarySearchTestPass()
+	{
+		
+		BinarySearchClass BSC = new  BinarySearchClass();
+		
+		int inputArray[] = {1, 2, 3, 4, 5, 6, 7, 8};
+		int input = 7;
+		boolean expected = true; 
+		boolean result  = BSC.isBinarySearch(inputArray, input);
+		assertEquals(expected, result); 
+		
+	}
+	
+	@Test 
+	public void binarySearchTestFail()
+	{
+		
+		BinarySearchClass BSC = new  BinarySearchClass();
+		
+		int inputArray[] = {1, 2, 3, 4, 5, 6, 7, 8};
+		int input = 11;
+		boolean expected = false; 
+		boolean result  = BSC.isBinarySearch(inputArray, input);
+		assertEquals(expected, result); 
+		
+	}
 }
+
