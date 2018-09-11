7a50a42c23f03d54f5925a175d08355e83d71b31
HEROMACHINE <Jesse.reyna01@utrgv.edu>
2018-09-11 00:18:55 -0500

    l

---
+++ LinearSearch/src/linearsearchpkg/LinearSearchTest.java
@@ -0,0 +1,14 @@
+package linearsearchpkg;
+
+public class LinearSearchTest {
+
+	@Test
+	public void linearSearch()
+	{
+		LinearSearchClass LSC = new  LinearSearchClass();
+		int input[] = {9, 3, 7, 2, 1};
+		  
+		
+		
+	}
+}
