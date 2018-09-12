d9499e5f5b3a7208b53951d916d24db8b16d3026
HEROMACHINE <Jesse.reyna01@utrgv.edu>
2018-09-11 19:16:51 -0500

    Testing

--- LinearSearch/src/linearsearchpkg/LinearSearchTest.java
+++ LinearSearch/src/linearsearchpkg/LinearSearchTest.java
@@ -1,14 +1,19 @@
 package linearsearchpkg;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class LinearSearchTest {
 
 	@Test
 	public void linearSearch()
 	{
 		LinearSearchClass LSC = new  LinearSearchClass();
-		int input[] = {9, 3, 7, 2, 1};
-		  
-		
-		
+		int inputArray[] = {9, 3, 7, 2, 1};
+		int input = 3;
+		boolean expected = true;
+		boolean result = LSC.foundLinearSearch(input, inputArray);
+		assertEquals(expected, result);		
 	}
 }
